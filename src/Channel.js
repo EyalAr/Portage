db3a88ec9db4b1b32ab9dd28ebe7dd030648e132
Eyal Arubas <eyalarubas@gmail.com>
2015-07-06 01:00:29 +0800

    depend on fuzzytree package

--- src/Channel.js
+++ src/Channel.js
@@ -1,158 +1,34 @@
 /* jshint esnext:true */
 
 import _ from 'lodash';
+import FuzzyTree from 'fuzzytree';
 import Subscription from './Subscription';
 
 class Channel{
     constructor(){
-        this._separator = '.';
-        this._wildcard = '*';
-        this._greedyWildcard = '#';
-        this._topicsDefs = {};
+        this._tree = new FuzzyTree();
     }
 
     subscribe(topic, cb){
-        if (_.isString(topic)) topic = topic.split(this._separator);
-        if (!_.isArray(topic)) throw Error("topic must be an array or a string");
         if (!_.isFunction(cb)) throw Error("callback must be a function");
-        if (!topic.length) throw Error("topic must contain at least one section");
-        if (!topic.every(_.isString)) throw Error("all topic sections must be strings");
 
-        var topicDef, i,
-            classifier = topic[0];
-
-        this._topicsDefs[classifier] = this._topicsDefs[classifier] || {
-            direct: [],
-            subtopics: {}
-        };
-
-        topicDef = this._topicsDefs[classifier];
-
-        for (i = 1 ; i < topic.length ; i++){
-            topicDef =
-            topicDef.subtopics[topic[i]] = topicDef.subtopics[topic[i]] || {
-                direct: [],
-                subtopics: {}
-            };
+        var node = this._tree.find(topic);
+        if (!node) {
+            node = this._tree.insert(topic);
+            node.setData([]);
         }
-
-        var s = new Subscription(cb, topicDef.direct);
-        topicDef.direct.push(s);
+        var s = new Subscription(cb, node.getData());
+        node.getData().push(s);
         return s;
     }
 
     publish(topic, ...data){
-        if (_.isString(topic)) topic = topic.split(this._separator);
-        if (!_.isArray(topic)) throw Error("topic must be an array or a string");
-        if (!topic.length) throw Error("topic must contain at least one section");
-        if (!topic.every(_.isString)) throw Error("all topic sections must be strings");
-        if (topic.some(isAnyWildcard.bind(this)))
-            throw Error("topic section cannot be a wildcard");
-
-        var subs = gatherSubscriptions.call(
-                this,
-                topic,
-                {
-                    direct: [],
-                    subtopics: this._topicsDefs
-                }
-            );
-
-        subs.forEach(s => s.invoke(data));
+        var nodes = this._tree.match(topic);
+        nodes.reduce((cbs, node) => {
+            _push(cbs, node.getData());
+            return cbs;
+        }, []).forEach(s => s.invoke(data));
     }
-}
-
-function isWildcard(s){
-    return s === this._wildcard;
-}
-
-function isGreedyWildcard(s){
-    return s === this._greedyWildcard;
-}
-
-function isAnyWildcard(s){
-    return isWildcard.call(this, s) || isGreedyWildcard.call(this, s);
-}
-
-function gatherSubscriptions(topic, topicsDefs){
-    var subtopic,
-        classifier = topic[0],
-        rest = topic.slice(1),
-        subs = [],
-        wildcard = this._wildcard,
-        greedyWildcard = this._greedyWildcard;
-
-    if (!rest.length){
-        for (subtopic in topicsDefs.subtopics){
-            if (topicsDefs.subtopics.hasOwnProperty(subtopic)){
-                if (
-                    subtopic === classifier ||
-                    subtopic === wildcard ||
-                    subtopic === greedyWildcard
-                ){
-                    _push(subs, topicsDefs.subtopics[subtopic].direct);
-                }
-            }
-        }
-    } else {
-        for (subtopic in topicsDefs.subtopics){
-            if (topicsDefs.subtopics.hasOwnProperty(subtopic)){
-                if (subtopic === greedyWildcard){
-                    _push(
-                        subs,
-                        gatherDeepSubscriptions.call(
-                            this,
-                            rest,
-                            topicsDefs.subtopics[subtopic]
-                        )
-                    );
-                } else if (subtopic === classifier || subtopic === wildcard){
-                    _push(
-                        subs,
-                        gatherSubscriptions.call(
-                            this,
-                            rest,
-                            topicsDefs.subtopics[subtopic]
-                        )
-                    );
-                }
-            }
-        }
-    }
-
-    return subs;
-}
-
-function gatherDeepSubscriptions(rest, topicsDefs){
-    var subtopic,
-        subs = [],
-        wildcard = this._wildcard,
-        greedyWildcard = this._greedyWildcard;
-
-    _push(subs, topicsDefs.direct);
-
-    for (subtopic in topicsDefs.subtopics){
-        if (topicsDefs.subtopics.hasOwnProperty(subtopic)){
-            var i = subtopic === greedyWildcard ? 0 : _.lastIndexOf(rest, subtopic);
-            if (i !== -1){
-                var def = {
-                    direct: [],
-                    subtopics: {}
-                };
-                def.subtopics[subtopic] = topicsDefs.subtopics[subtopic];
-                _push(
-                    subs,
-                    gatherSubscriptions.call(
-                        this,
-                        rest.slice(i),
-                        def
-                    )
-                );
-            }
-        }
-    }
-
-    return subs;
 }
 
 function _push(target, elements){
